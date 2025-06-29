c498a99d8e58120aabba085e8af91d862b3aea89
Tanmay <tanmaytambat01@gmail.com>
2025-06-29 23:29:34 +0530

    add test and configured github actions

--- f_posts/tests.py
+++ f_posts/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
+from f_posts.models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class BaseSetup(TestCase):
+    def setUp(self):
+        user_data = {
+            "email":"test@gmail.com",
+            "username":"testuser",
+            "password1":"testpassword123",
+            "password2":"testpassword123"
+        }
+        self.client.post(reverse("account_signup"), user_data, format="text/html")
+
+class PostCreateTest(BaseSetup):
+    def test_post_create(self):
+        response = self.client.get(reverse("post-create"))
+        self.assertEqual(response.status_code, 200)
+        
+        form_data = {
+            "url" : "https://example.com",
+            "body" : "This is a test post",
+        }
+         
+         
+        self.user = User.objects.get(username="testuser")
+        post_data = {
+            'url': form_data['url'],
+            'body': form_data['body'],
+            'title': 'Test Post',
+            'image': 'https://picsum.photos/200/300',
+            'artist': 'Test Artist',
+            'author': self.user          
+        }
+        
+        post = Post.objects.create(**post_data)
+        self.assertEqual(Post.objects.filter(title=post.title).exists(), True )
+        
+        homepage = self.client.get(reverse("home"))
+        self.assertContains(homepage, post.title)
+        
+        post.delete()
+        response = self.client.get(reverse("home"))
+        self.assertNotContains(response, post.title)
+        
--- f_users/tests.py
+++ f_users/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User 
+from f_users.models import Profile
 
-# Create your tests here.
+class BaseSetup(TestCase):
+    def setUp(self):
+        user_data = {
+            "email":"test@gmail.com",
+            "username":"testuser",
+            "password1":"testpassword123",
+            "password2":"testpassword123"
+        }
+        self.client.post(reverse("account_signup"), user_data, format="text/html")
+
+
+class SignUpTest(TestCase):
+    def test_sign_up_page_exits(self):
+        response = self.client.get(reverse("account_signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+        
+    def test_sign_up_user(self):
+        user_data = {
+            "email":"test@gmail.com",
+            "username":"testuser",
+            "password1":"testpassword123",
+            "password2":"testpassword123"
+        }
+        response = self.client.post(reverse("account_signup"), user_data, format="text/html")
+        self.assertEqual(response.status_code, 302)  # Should redirect after successful signup
+        
+        response = self.client.get(reverse("profile-view"), args=[user_data["username"]])
+        self.assertEqual(response.status_code, 200)
+    
+class ProfileEditTest(BaseSetup):
+    def test_profile_edit(self):
+        response = self.client.get(reverse("profile-edit"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "f_users/profile-edit.html")
+        
+        test_mail = "test2@gmail.com"
+        form_data = {
+            "email": test_mail
+        }
+        response = self.client.post(reverse("profile-edit"), form_data, format="text/html")
+        self.assertEqual(response.status_code, 302)
+        
+        user_email = User.objects.get(username="testuser").email
+        self.assertEqual(user_email, test_mail )
+        profile_email = Profile.objects.get(user__username="testuser").email
+        self.assertEqual(profile_email, test_mail)
+        
+        
