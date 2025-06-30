16126996a2d02fde137e155bb995eae1482f5569
Tanmay <tanmaytambat01@gmail.com>
2025-06-30 18:48:58 +0530

    fix sitemaps

--- flicksta/urls.py
+++ flicksta/urls.py
@@ -22,7 +22,7 @@ sitemaps ={
 
 
 urlpatterns = [
-    path("sitemap.xml/", sitemap, {'sitemaps':sitemaps }, name="django.contrib.sitemaps.urls.views.sitemap"),
+    path("sitemap.xml/", sitemap, {'sitemaps':sitemaps }, name="django.contrib.sitemaps.views.sitemap"),
     path("robots.txt/", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots.txt"), 
     path("theboss/", admin.site.urls),
     path('accounts/', include('allauth.urls')),
